9a3ad85563ad8950bfa7afa53dfd64046d7ccfd5
Olga Solodovnikova <ossolodovnikova@yandex.ru>
2024-05-17 23:35:10 +0300

    fix: test token

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -17,7 +17,6 @@ export const register = (email, password) => {
     return fetch(`${BASE_URL}/signup`, {
         method: 'POST',
         headers: {
-            'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
@@ -32,7 +31,6 @@ export const login = (email, password) => {
     return fetch(`${BASE_URL}/signin`, {
         method: 'POST',
         headers: {
-            'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
@@ -43,13 +41,13 @@ export const login = (email, password) => {
         .then((res) => handleResponse(res))
 }
 
-export const getContent = () => {
+export const getContent = (token) => {
     return fetch(`${BASE_URL}/users/me`, {
         method: 'GET',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
-            // 'Authorization': `Bearer ${token}`,
+            'Authorization': `Bearer ${token}`,
         }
     })
         .then((res) => handleResponse(res))
